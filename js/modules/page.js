1b933995a95b7792c97835f9e0299e06d5ab9405
cnleafar <85846828+cnleafar@users.noreply.github.com>
2021-07-22 19:47:39 -0300

    js add modules

---
+++ js/modules/page.js
@@ -0,0 +1,51 @@
+function initPage() {
+
+    const home = document.querySelector('.js-home');
+
+    const contact = document.querySelector('.js-contact');
+
+    const menu = document.querySelector('.c-main-menu__list');
+
+    menu.addEventListener('click',(item) => {
+
+        const page = item.target.dataset.target;
+
+        // menu.nextElementSibling.forEach((page)=> console.log(page) );
+
+        if(page) {
+
+            const pages = document.querySelectorAll('[data-page]');
+
+            const pageActive = document.querySelector(`[data-page=${page}]`); 
+
+
+            if(pageActive.classList.contains('active') === false) {
+
+                pages.forEach((page)=> {
+                    if(page.classList.contains('active')) {
+
+                        const targetPage = document.querySelector(`[data-target=${page.dataset.page}]`);
+                        targetPage.classList.remove('active');
+                        
+                    }
+                    page.classList.remove('active');
+                });
+
+                if(!pageActive.classList.contains('active')) {
+
+                    item.target.classList.add('active');
+
+                    pageActive.classList.add('active');
+
+                }
+
+            }
+
+
+        }
+
+    });
+
+}
+
+export default initPage;
