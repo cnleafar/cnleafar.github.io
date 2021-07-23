1d2822f69110c1b091d1d5f206a7b183ba880fa9
cnleafar <85846828+cnleafar@users.noreply.github.com>
2021-07-23 00:17:40 -0300

    Update main.js

--- js/modules/main.js
+++ js/modules/main.js
@@ -62,41 +62,41 @@ const swiper = new Swiper('.swiper-container', {
 // window.addEventListener('load',() => document.getElementById("loading").style.display = "none");
 
 
-// Store a copy of the fetch function
-var _oldFetch = fetch; 
+// // Store a copy of the fetch function
+// var _oldFetch = fetch; 
 
-// Create our new version of the fetch function
-window.fetch = function(){
+// // Create our new version of the fetch function
+// window.fetch = function(){
 
-    // Create hooks
-    var fetchStart = new Event( 'fetchStart', { 'view': document, 'bubbles': true, 'cancelable': false } );
-    var fetchEnd = new Event( 'fetchEnd', { 'view': document, 'bubbles': true, 'cancelable': false } );
+//     // Create hooks
+//     var fetchStart = new Event( 'fetchStart', { 'view': document, 'bubbles': true, 'cancelable': false } );
+//     var fetchEnd = new Event( 'fetchEnd', { 'view': document, 'bubbles': true, 'cancelable': false } );
     
-    // Pass the supplied arguments to the real fetch function
-    var fetchCall = _oldFetch.apply(this, arguments);
+//     // Pass the supplied arguments to the real fetch function
+//     var fetchCall = _oldFetch.apply(this, arguments);
     
-    // Trigger the fetchStart event
-    document.dispatchEvent(fetchStart);
+//     // Trigger the fetchStart event
+//     document.dispatchEvent(fetchStart);
     
-    fetchCall.then(function(){
-        // Trigger the fetchEnd event
-        document.dispatchEvent(fetchEnd);
-    }).catch(function(){
-        // Trigger the fetchEnd event
-        document.dispatchEvent(fetchEnd);
-    });
+//     fetchCall.then(function(){
+//         // Trigger the fetchEnd event
+//         document.dispatchEvent(fetchEnd);
+//     }).catch(function(){
+//         // Trigger the fetchEnd event
+//         document.dispatchEvent(fetchEnd);
+//     });
     
-    return fetchCall;
-};
+//     return fetchCall;
+// };
 
-document.addEventListener('fetchStart', function() {
-    console.log("Show spinner");
-});
+// document.addEventListener('fetchStart', function() {
+//     console.log("Show spinner");
+// });
 
-document.addEventListener('fetchEnd', function() {
-    console.log("Hide spinner");
-});
+// document.addEventListener('fetchEnd', function() {
+//     console.log("Hide spinner");
+// });
 
-fetch('https://ipinfo.io/json');
-fetch('https://ipinfo.io/json');
-fetch('https://ipinfo.io/json');
+// fetch('https://ipinfo.io/json');
+// fetch('https://ipinfo.io/json');
+// fetch('https://ipinfo.io/json');
