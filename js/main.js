5f71421c7f7906e00d5e684521dece31cdf86061
cnleafar <85846828+cnleafar@users.noreply.github.com>
2021-07-22 23:52:56 -0300

    add typewriter effect

--- js/main.js
+++ js/main.js
@@ -1,6 +1,9 @@
 import Site from './modules/main.js';
 import initPage from './modules/page.js';
+import initEscrever from './modules/escrever.js';
 
+const titlePag = document.querySelector('.c-header__title');
 
+initEscrever('Trabalhos Recentes',titlePag);
 Site();
 initPage();
