9cbd8794c68dd2d1e7247899ca519e8562b4c615
cnleafar <85846828+cnleafar@users.noreply.github.com>
2021-07-23 00:35:48 -0300

    fix v2

--- js/modules/escrever.js
+++ js/modules/escrever.js
@@ -1,23 +1,14 @@
 export default function initEscrever(text) {
 
-    console.log(text);
-
     const textSplit = text.innerText.split('');
-    // console.log(textSplit);
-
-    // console.log(textSplit);
     
     text.textContent = '';
 
     textSplit.forEach((letter,i) => {
-//         console.log(letter);
         setTimeout(function() {
 
         text.textContent += letter;
 
        },40 * i);
     });
-
-    // clearInterval(teste);
-
 }
