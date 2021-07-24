cda728590893f0e7bd6f9689b41b613b9911d660
cnleafar <85846828+cnleafar@users.noreply.github.com>
2021-07-24 19:41:59 -0300

    refactoring typewriter

--- js/modules/escrever.js
+++ js/modules/escrever.js
@@ -1,14 +1,21 @@
+
+let intervalWriter;
+
 export default function initEscrever(text) {
-
     const textSplit = text.innerText.split('');
+    text.textContent = null;
+    let i = 0;
+    clearInterval(intervalWriter);
     
-    text.textContent = '';
-
-    textSplit.forEach((letter,i) => {
-        setTimeout(function() {
-
-        text.textContent += letter;
-
-       },40 * i);
-    });
+    function typeWriter() {
+        
+        if (i <= (textSplit.length - 1)) {
+            text.textContent += textSplit[i];
+        } else {
+            clearInterval(intervalWriter);
+        }
+        
+        ++i;
+    }
+    intervalWriter = setInterval(typeWriter, 1000 / textSplit.length);
 }
